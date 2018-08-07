c0094618d37a5f4a72f611fe0fea3b81ca3eb22e
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-08-07 17:23:43 +0000

    padding completed for now

--- project/createpadding.c
+++ project/createpadding.c
@@ -6,11 +6,36 @@
 /*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 13:31:41 by pstubbs           #+#    #+#             */
-/*   Updated: 2018/08/07 13:33:18 by pstubbs          ###   ########.fr       */
+/*   Updated: 2018/08/07 17:22:04 by pstubbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+char	*specialpaddingfordigit(t_format *format, int len, char c ,char **str)
+{
+		char	*amstr;
+		char	*tmp;
+		int		isneg;
+
+		if (*str[0] == '-' && format->minus == 0)
+		{
+			tmp = ft_strdup("-");
+			*str[0] = '0';
+		}
+		else if (*str[0] != '-' && (format->spacpad == 1 && format->zeropad == 1))
+			tmp = ft_strdup(" ");
+		else
+			tmp = ft_strdup("0");
+		amstr = ft_strnew(format->padsize - len - 1);
+		ft_memset(amstr, c, format->padsize - len - 1);
+		tmp = dynamicstring(&tmp ,amstr);
+		free(amstr);
+		amstr = ft_strdup(tmp);
+		free(tmp);
+		return (amstr);
+}
+
 
 char	*createpadding(t_printf *node, char **str, t_format *format)
 {
@@ -23,24 +48,19 @@ char	*createpadding(t_printf *node, char **str, t_format *format)
 	if (format->padsize <= len)
 		return (*str);
 	c = ' ';
-	if (format->zeropad == 1)
+	if (format->zeropad == 1 && format->minus == 0)
 		c = '0';
-	if (format->spacpad == 1 && format->zeropad == 1 && (format->c == 'd' || format->c == 'D' || format->c == 'i'))
-	{
-		tmp = ft_strdup(" ");
-		amstr = ft_strnew(format->padsize - len - 1);
-		ft_memset(amstr, c, format->padsize - len - 1);
-		tmp = dynamicstring(&tmp ,amstr);
-		free(amstr);
-		amstr = ft_strdup(tmp);
-		free(tmp);
-	}
+	if ((((format->spacpad == 1 && format->zeropad == 1) || (format->zeropad == 1 && format->spacpad == 0) ) && (format->c == 'd' || format->c == 'D' || format->c == 'i')) && format->minus == 0)
+		amstr = specialpaddingfordigit(format, len, c, str);
 	else
 	{
 		amstr = ft_strnew(format->padsize - len);
 		ft_memset(amstr, c, format->padsize - len);
 	}
-	tmp = ft_strjoin(amstr, *str);
+	if (format->minus == 1)
+		tmp = ft_strjoin(*str, amstr);
+	else
+		tmp = ft_strjoin(amstr, *str);
 	free(*str);
 	*str = ft_strdup(tmp);
 	free(amstr);

dd14c480ed7537e7510e112e429084df2fcbef21
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-08-15 19:16:14 +0200

    findoct normed

--- project/src/findoct.c
+++ project/src/findoct.c
@@ -6,7 +6,7 @@
 /*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/09 10:51:38 by pstubbs           #+#    #+#             */
-/*   Updated: 2018/08/15 11:15:05 by pstubbs          ###   ########.fr       */
+/*   Updated: 2018/08/15 19:15:19 by pstubbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,41 +43,35 @@ void	hashflagoct(char **tmpstr)
 	free(tmpstr2);
 }
 
+void	findoctbody(t_format *format, long long tmp, char **tmpstr)
+{
+	if (format->lenmod == 1 || format->c == 'O')
+		*tmpstr = lengthmodoct(format, tmp, 0);
+	else
+		*tmpstr = ft_itoa_base((unsigned int)tmp, 8, 0);
+	if ((tmp > 0 && format->hash == 1) && ((format->spacpad == 0 &&
+	format->zeropad == 0) || (tmp > format->precsize)))
+		hashflagoct(tmpstr);
+	if (format->prec == 1)
+		*tmpstr = precision(format, tmpstr);
+}
+
 int		findoct(t_printf *node, va_list args, t_format *format)
 {
 	long long	tmp;
-	char	*tmpstr;
-	// char	*tmpstr2;
+	char		*tmpstr;
 
 	if (format->wild > 0)
 		wildcard(node, format, args);
 	tmp = va_arg(args, long long);
-	if (tmp == 0 && format->prec == 1 && format->precsize == 0 && format->padsize == 0 && format->hash == 0)
+	if (tmp == 0 && format->prec == 1 && format->precsize == 0 &&
+	format->padsize == 0 && format->hash == 0)
 		return (0);
-	else if (tmp == 0 && format->prec == 1 && format->precsize == 0 && format->padsize != 0)
+	else if (tmp == 0 && format->prec == 1 && format->precsize == 0 &&
+	format->padsize != 0)
 		tmpstr = ft_strdup(" ");
 	else
-	{
-		if (format->lenmod == 1 || format->c == 'O')
-			tmpstr = lengthmodoct(format, tmp, 0);
-		else
-			tmpstr = ft_itoa_base((unsigned int)tmp, 8, 0);
-		if ( (tmp > 0 && format->hash == 1) && ((format->spacpad == 0 && format->zeropad == 0) || (tmp > format->precsize)))
-			hashflagoct(&tmpstr);
-		if (format->prec == 1)
-			tmpstr = precision(format, &tmpstr);
-		
-	}
-	// if ( (tmp > 0 && format->hash == 1) && ((format->spacpad == 0 && format->zeropad == 0) || (tmp > format->precsize)))
-	// if ( (tmp > 0 && format->hash == 1) && ((format->spacpad == 0 && format->zeropad == 0) && format->prec == 0))
-	// {
-	// 	hashflagoct(&tmpstr);
-		// tmpstr2 = ft_strjoin("0", tmpstr);
-		// free(tmpstr);
-		// tmpstr = ft_strdup(tmpstr2);
-		// free(tmpstr2);
-	// }
-	
+		findoctbody(format, tmp, &tmpstr);
 	if (format->spacpad == 1 || format->zeropad == 1)
 		tmpstr = createpadding(&tmpstr, format);
 	node->output = dynamicstring(&(node)->output, tmpstr);
